b41f295ffaba446de392c3e8945194b94a16a9ab
lanlacope <pokeuni150@gmail.com>
2024-11-11 02:05:36 +0900

    アニメーション付き関数を追加

--- lanlacopelib/src/main/java/io/github/lanlacope/lanlacopelib/composeable/ui/busy/BusyItem.kt
+++ lanlacopelib/src/main/java/io/github/lanlacope/lanlacopelib/composeable/ui/busy/BusyItem.kt
@@ -66,6 +66,66 @@ inline fun <T> BusyListScope.itemsIndexed(
     itemContent(it, items[it])
 }
 
+inline fun <T> BusyListScope.animatedItems(
+    items: List<T>,
+    noinline key: ((item: T) -> Any)? = null,
+    noinline contentType: (item: T) -> Any? = { null },
+    crossinline itemContent: @Composable BusyItemScope.(item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(items[index]) } else null,
+    contentType = { index: Int -> contentType(items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(items[it])
+    }
+}
+
+inline fun <T> BusyListScope.animatedItemsIndexed(
+    items: List<T>,
+    noinline key: ((index: Int, item: T) -> Any)? = null,
+    crossinline contentType: (index: Int, item: T) -> Any? = { _, _ -> null },
+    crossinline itemContent: @Composable BusyItemScope.(index: Int, item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(index, items[index]) } else null,
+    contentType = { index -> contentType(index, items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(it, items[it])
+    }
+}
+
+inline fun <T> BusyListScope.animatedItems(
+    items: Array<T>,
+    noinline key: ((item: T) -> Any)? = null,
+    noinline contentType: (item: T) -> Any? = { null },
+    crossinline itemContent: @Composable BusyItemScope.(item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(items[index]) } else null,
+    contentType = { index: Int -> contentType(items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(items[it])
+    }
+}
+
+inline fun <T> BusyListScope.animatedItemsIndexed(
+    items: Array<T>,
+    noinline key: ((index: Int, item: T) -> Any)? = null,
+    crossinline contentType: (index: Int, item: T) -> Any? = { _, _ -> null },
+    crossinline itemContent: @Composable BusyItemScope.(index: Int, item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(index, items[index]) } else null,
+    contentType = { index -> contentType(index, items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(it, items[it])
+    }
+}
+
 @Stable
 @LazyScopeMarker
 interface BusyItemScope {
