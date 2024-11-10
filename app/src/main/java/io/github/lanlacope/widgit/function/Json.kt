86ba5a24176bdb657e43d58a1f8046ea7de189a3
lanlacope <pokeuni150@gmail.com>
2024-11-10 18:24:28 +0900

    変数名の整理

--- app/src/main/java/io/github/lanlacope/widgit/function/Json.kt
+++ app/src/main/java/io/github/lanlacope/widgit/function/Json.kt
@@ -3,11 +3,11 @@ package io.github.lanlacope.widgit.function
 import org.json.JSONArray
 
 @Suppress("unused")
-inline fun JSONArray.forEachIndexOnly(action: (Int) -> Unit) {
+inline fun JSONArray.indexedForEach(action: (Int) -> Unit) {
     for (index in 0 until length()) action(index)
 }
 
 @Suppress("unused")
-inline fun <R> JSONArray.mapIndexOnly(action: (Int) -> R): List<R> {
+inline fun <R> JSONArray.indexedMap(action: (Int) -> R): List<R> {
     return (0 until length()).map { index -> action(index) }
 }
