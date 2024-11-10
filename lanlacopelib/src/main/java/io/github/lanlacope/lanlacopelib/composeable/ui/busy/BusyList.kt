7319b1e17801f78f1ed12503515cfc8c0e283b8b
lanlacope <pokeuni150@gmail.com>
2024-11-11 01:21:00 +0900

    BusyListをinternalに

--- lanlacopelib/src/main/java/io/github/lanlacope/lanlacopelib/composeable/ui/busy/BusyList.kt
+++ lanlacopelib/src/main/java/io/github/lanlacope/lanlacopelib/composeable/ui/busy/BusyList.kt
@@ -1,17 +1,9 @@
 package io.github.lanlacope.widgit.composeable.ui.busy
 
-import androidx.annotation.FloatRange
-import androidx.compose.animation.core.FiniteAnimationSpec
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.VisibilityThreshold
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyScopeMarker
 import androidx.compose.runtime.Composable
@@ -19,10 +11,9 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.IntOffset
 
 @Composable
-fun BusyList(
+internal fun BusyList(
     modifier: Modifier,
     // state: LazyListState = rememberLazyListState(),
     contentPadding: PaddingValues,
