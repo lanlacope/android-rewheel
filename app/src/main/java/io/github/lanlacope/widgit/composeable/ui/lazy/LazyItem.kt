c397279c969587635a288e33d882e12eb55136ee
lanlacope <pokeuni150@gmail.com>
2024-11-10 17:43:51 +0900

    オーバーライドの整理

---
+++ app/src/main/java/io/github/lanlacope/widgit/composeable/ui/lazy/LazyItem.kt
@@ -0,0 +1,66 @@
+package io.github.lanlacope.widgit.composeable.ui.lazy
+
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Suppress("unused")
+inline fun <T> LazyListScope.animatedItems(
+    items: List<T>,
+    noinline key: ((item: T) -> Any)? = null,
+    noinline contentType: (item: T) -> Any? = { null },
+    crossinline itemContent: @Composable LazyItemScope.(item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(items[index]) } else null,
+    contentType = { index: Int -> contentType(items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(items[it])
+    }
+}
+
+@Suppress("unused")
+inline fun <T> LazyListScope.animatedItemsIndexed(
+    items: List<T>,
+    noinline key: ((index: Int, item: T) -> Any)? = null,
+    crossinline contentType: (index: Int, item: T) -> Any? = { _, _ -> null },
+    crossinline itemContent: @Composable LazyItemScope.(index: Int, item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(index, items[index]) } else null,
+    contentType = { index -> contentType(index, items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(it, items[it])
+    }
+}
+
+inline fun <T> LazyListScope.animatedItems(
+    items: Array<T>,
+    noinline key: ((item: T) -> Any)? = null,
+    noinline contentType: (item: T) -> Any? = { null },
+    crossinline itemContent: @Composable LazyItemScope.(item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(items[index]) } else null,
+    contentType = { index: Int -> contentType(items[index]) }
+) {
+    androidx.compose.foundation.layout.Box(modifier = Modifier.animateItem()) {
+        itemContent(items[it])
+    }
+}
+
+inline fun <T> LazyListScope.animatedItemsIndexed(
+    items: Array<T>,
+    noinline key: ((index: Int, item: T) -> Any)? = null,
+    crossinline contentType: (index: Int, item: T) -> Any? = { _, _ -> null },
+    crossinline itemContent: @Composable LazyItemScope.(index: Int, item: T) -> Unit
+) = items(
+    count = items.size,
+    key = if (key != null) { index: Int -> key(index, items[index]) } else null,
+    contentType = { index -> contentType(index, items[index]) }
+) {
+    itemContent(it, items[it])
+}
