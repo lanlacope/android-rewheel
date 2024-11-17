97d56dc12c4fe14a9b54f23a017facb06d9cd191
lanlacope <pokeuni150@gmail.com>
2024-11-18 01:24:21 +0900

    不必要な引数を削除

--- rewheel/src/main/java/io/github/lanlacope/rewheel/composeable/ui/action/setting/SettingTextArea.kt
+++ rewheel/src/main/java/io/github/lanlacope/rewheel/composeable/ui/action/setting/SettingTextArea.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
