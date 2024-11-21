a67065ca63f221931c244fd051b7f6a077c9fc0a
lanlacope <pokeuni150@gmail.com>
2024-11-21 15:55:27 +0900

    使用例の更新

--- lanlacopetest/src/main/java/io/github/lanlacope/lanlacopetest/example/busy/option/BusyOption.kt
+++ lanlacopetest/src/main/java/io/github/lanlacope/lanlacopetest/example/busy/option/BusyOption.kt
@@ -4,29 +4,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.lanlacope.lanlacopetest.ui.theme.WidgitTheme
 import io.github.lanlacope.rewheel.composeable.ui.busy.option.BusyOption
 import io.github.lanlacope.rewheel.composeable.ui.busy.option.checkBox
+import io.github.lanlacope.rewheel.composeable.ui.busy.option.radioButton
 import io.github.lanlacope.rewheel.function.toggle
 
 @Composable
 internal fun ExampleBusyOption() {
 
-    val options = remember { mutableStateMapOf("A" to "AAA", "B" to "BBB", "C" to "CCC") }
-    val checked = remember { mutableStateListOf("A") }
+    val checkBoxOptions = remember { mutableStateListOf("A", "B", "C") }
+    val checkedOptions = remember { mutableStateListOf("A") }
+
+    val radioButtonOptions = remember { mutableStateListOf(1, 2, 3) }
+    var selectedOption by remember { mutableStateOf(1) }
 
     BusyOption(modifier = Modifier.fillMaxWidth()) {
         checkBox(
-            options = options,
-            checked = { checked.contains(it) },
-            onClick = { checked.toggle(it) }
+            options = checkBoxOptions.associateWith { "Option: $it" },
+            checked = { checkedOptions.contains(it) },
+            onClick = { checkedOptions.toggle(it) }
         )
 
+        radioButton(
+            options = radioButtonOptions.associateWith { "Option: $it" },
+            selected = { selectedOption == it },
+            onClick = { selectedOption = it }
+        )
     }
 }
 
