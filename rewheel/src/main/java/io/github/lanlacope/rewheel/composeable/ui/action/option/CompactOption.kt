f90d8a5bf19cec7b2b5beaee3574d02eaedb7e85
lanlacope <pokeuni150@gmail.com>
2024-11-24 17:58:21 +0900

    CompactOptionを実装

---
+++ rewheel/src/main/java/io/github/lanlacope/rewheel/composeable/ui/action/option/CompactOption.kt
@@ -0,0 +1,55 @@
+package io.github.lanlacope.rewheel.composeable.ui.action.option
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun CompactOptionCheckBox(
+    modifier: Modifier = Modifier,
+    text: String,
+    checked: Boolean,
+    onClick: () -> Unit
+) {
+    OptionCheckBox(
+        modifier = modifier,
+        text = text,
+        checked = checked,
+        textStyle = TextStyle(),
+        onClick = onClick,
+        innerPadding = PaddingValues()
+    )
+}
+
+@Composable
+fun CompactOptionOptionRadioButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    OptionRadioButton(
+        modifier = modifier,
+        text = text,
+        selected = selected,
+        textStyle = TextStyle(),
+        onClick = onClick,
+        innerPadding = PaddingValues(),
+    )
+}
+
+@Composable
+fun CompactOptionText(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
+    OptionText(
+        modifier = modifier,
+        text = text,
+        textStyle = TextStyle(),
+        onClick = onClick,
+        innerPadding = PaddingValues()
+    )
+}
