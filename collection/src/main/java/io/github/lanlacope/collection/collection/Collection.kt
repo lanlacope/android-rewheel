7966da88fbc014d05ee1a1475bb11306f03fe71c
lanlacope <pokeuni150@gmail.com>
2024-11-28 16:52:18 +0900

    ディレクトリを整理

--- collection/src/main/java/io/github/lanlacope/collection/collection/Collection.kt
+++ collection/src/main/java/io/github/lanlacope/collection/collection/Collection.kt
@@ -1,11 +1,5 @@
 package io.github.lanlacope.collection.collection
 
-fun <T> Iterable<T>.toArrayList(): ArrayList<T> {
-    if (this is Collection<T>)
-        return this.toArrayList()
-    return toCollection(ArrayList<T>())
-}
-
 fun <T> Collection<T>.toArrayList(): ArrayList<T> {
     return ArrayList(this)
 }
