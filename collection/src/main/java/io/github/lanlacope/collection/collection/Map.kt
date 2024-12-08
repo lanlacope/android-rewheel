5b9f612889581b9cce74df3c5d1fe2372d4a7940
lanlacope <pokeuni150@gmail.com>
2024-12-08 15:01:19 +0900

    toMutableStateMapの追加

--- collection/src/main/java/io/github/lanlacope/collection/collection/Map.kt
+++ collection/src/main/java/io/github/lanlacope/collection/collection/Map.kt
@@ -1,9 +1,16 @@
 package io.github.lanlacope.collection.collection
 
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.snapshots.SnapshotStateMap
+
 fun <K, V> Map<K, V>.keyList(): List<K> {
     return this.keys.toList()
 }
 
 fun <K, V> Map<K, V>.forEachIndexed(action: (index: Int, Map.Entry<K, V>) -> Unit) {
     this.entries.forEachIndexed { index, entry ->  action(index, entry) }
+}
+
+fun <K, V> Map<K, V>.toMutableStateMap(): SnapshotStateMap<K, V> {
+    return mutableStateMapOf<K, V>().apply { putAll(this@toMutableStateMap) }
 }
