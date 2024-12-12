f4491bf2290ae4d7e2cdac15b58013988f03e0f0
lanlacope <pokeuni150@gmail.com>
2024-12-12 19:09:26 +0900

    動作を調整

--- compose/src/main/java/io/github/lanlacope/compose/ui/button/layout/ManuButton.kt
+++ compose/src/main/java/io/github/lanlacope/compose/ui/button/layout/ManuButton.kt
@@ -1,6 +1,7 @@
 package io.github.lanlacope.compose.ui.button.layout
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -11,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,7 +27,7 @@ fun ManuButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    color: Color = MaterialTheme.colorScheme.onBackground,
+    colors: ManuButtonColors = ManuButtonDefaults.colors(),
     content: @Composable ColumnScope.() -> Unit,
 ) {
     BoxButton(
@@ -37,21 +37,22 @@ fun ManuButton(
         modifier = modifier
             .padding(all = 10.dp)
             .clip(RoundedCornerShape(4.dp))
-            .border(BorderStroke(1.dp, Color.LightGray), RoundedCornerShape(4.dp))
+            .border(BorderStroke(1.dp, colors.borderColor), RoundedCornerShape(4.dp))
+            .background(colors.containerColor)
 
     ) {
         Column {
             Row {
                 Text(
                     text = text,
-                    style = TextStyle(color = color),
+                    style = TextStyle(color = colors.textColor),
                     modifier = Modifier
                         .align(Alignment.CenterVertically)
                 )
 
                 Icon(
                     imageVector = Icons.Filled.ArrowDropDown,
-                    tint = color,
+                    tint = colors.textColor,
                     contentDescription = "contentDescription",
                     modifier = Modifier.align(Alignment.CenterVertically)
                 )
