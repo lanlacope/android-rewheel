7d8b0e8b19e2212e46d63c5cd0e20bdf0addd8a4
lanlacope <pokeuni150@gmail.com>
2024-12-12 22:42:14 +0900

    未使用の削除

--- compose/src/main/java/io/github/lanlacope/compose/ui/text/manu/OutlinedTextFieldManu.kt
+++ compose/src/main/java/io/github/lanlacope/compose/ui/text/manu/OutlinedTextFieldManu.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
