d9182dee606c45adcb32e373a89cfd92561a88a1
lanlacope <pokeuni150@gmail.com>
2024-12-13 16:51:16 +0900

    publish compose-1.2.4

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "io.github.lanlacope.android-rewheel"
             artifactId = "compose"
-            version = "1.2.3"
+            version = "1.2.4"
 
             afterEvaluate {
                 from(components["release"])
