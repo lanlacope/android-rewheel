5ae00d5c07d469ad37203e259c8ecdab5d3bde62
lanlacope <pokeuni150@gmail.com>
2025-03-13 18:41:26 +0900

    多機能ツールバーを実装

--- compose/src/main/java/io/github/lanlacope/compose/ui/text/toolbar/DynamicTextToolbarDefaults.kt
+++ compose/src/main/java/io/github/lanlacope/compose/ui/text/toolbar/DynamicTextToolbarDefaults.kt
@@ -9,5 +9,9 @@ object DynamicTextToolbarDefaults {
         const val PASTE = "Paste"
         const val CUT = "Cut"
         const val SELECT_ALL = "Select All"
+        const val SHARE = "Share"
+        const val SEARCH = "Search"
+        const val TRANSLATE = "Translate"
+        const val BROWSER = "Open In Browser"
     }
 }
